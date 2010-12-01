4910ce8fb5eaee6c156c0d732e87a03f5e008b3f
Andrzej Byczko <veriservus@gmail.com>
2010-12-01 17:59:13 +0000

    tests fixed

--- test/clork_test.clj
+++ test/clork_test.clj
@@ -15,7 +15,7 @@
                                             "study")}
                      :players {:player1 (struct player :hall [])
                                :player2 (struct player :kitchen [])}
-                     :mobiles { :orc {:description "Angry Orc" :location :kitchen :think (fn [world] (println "Hmmmm") world)}
+                     :mobiles { :orc {:description "Angry Orc" :location :kitchen :think (fn [world] (println "Hmmmm") world)}}
                      :items {:sword (struct item "A very pointy sword.")}})
   (f))
 
